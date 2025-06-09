c77bbd89de993c301dc7f4097c18c458869b9804
iamabhishekagarwal <iamabhishekagarwal1999@gmail.com>
2025-06-09 23:58:09 +0530

    Added cookies backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./Pages/Home"
 import SignIn from "./Pages/Signin"
 import Signup from "./Pages/Signup"
 import {BrowserRouter,Routes,Route} from "react-router-dom"
@@ -6,6 +7,7 @@ function App() {
     <BrowserRouter>
     <div>
       <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
         <Route path="/signin" element={<SignIn></SignIn>}></Route>
       </Routes>
